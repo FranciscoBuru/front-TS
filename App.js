cd2f4a58d580e465c26f43343eaf9e4d70162309
Francisco Aramburu <fran@Franciscos-MacBook-Pro.local>
2022-09-25 11:47:30 -0500

    same

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
+
 import './App.css';
-import DashboardAdmin from './dashboardadmin';
 import NavBar from './NavBar';
-import DashboardUser from './dashboarduser';
-import DashboardCommunity from './dashboardcommunity';
+
+
 import Formcommunity from './formcommunity';
 import RegisterPage from './registerpage';
 import Formuser from './formuser';
@@ -20,10 +20,6 @@ function App() {
       <Route path='/formuser' element={<Formuser/>}/>
       <Route path='/formcommunity' element={<Formcommunity/>}/>
       <Route path='/registerpage' element={<RegisterPage/>}/>
-      <Route path='/dashboardcommunity' element={<DashboardCommunity/>}/>
-      <Route path='/dashboarduser' element={<DashboardUser/>}/>
-      <Route path='/dashboardadmin' element={<DashboardAdmin/>}/>
-
     </Routes>
     </>
 
